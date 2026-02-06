c241c72af986f54ec59e34a6acf4a5081f84964d
siddhi0092 <siddhirajput1980@gmail.com>
2026-02-06 16:16:18 +0530

    completed conditional and started with functions

---
+++ try.py
@@ -0,0 +1,13 @@
+# n = 6
+# print(function(n))
+
+# def function(n):
+#     return n+1
+
+#the above code gives function not defined error
+
+def function(n):
+    return n+1 
+
+n = 6
+print(function(n))
